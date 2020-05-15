9bd4fc45e9cc1ce390e92d8d53259ffa66733b3c
Deepanshu Dhruw <deepanshu.dhruw@outlook.com>
2020-05-15 20:09:15 +0530

    HACKERMODE ++++

---
+++ playerA.js
@@ -0,0 +1,214 @@
+// 'use strict'
+let gamePiece;
+function startGame() {
+    gameArea.start();
+    gamePiece = new gameBall(200, gameArea.canvas.height -  250, ballColor); 
+}
+//........GAME AREA LOAD.........//
+let gameArea = {
+    canvas : document.createElement('canvas'),
+    start : function() {
+        this.canvas.width = 400;
+        this.canvas.height = window.innerHeight;
+        this.canvas.style.backgroundColor = 'rgb(59, 59, 59)';
+        this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        requestAnimationFrame(updateGameArea);
+        window.addEventListener('keydown', function (e) {
+            gameArea.keys = (gameArea.keys || []);
+            gameArea.keys[e.keyCode] = true;
+        })
+        window.addEventListener('keyup', function (e) {
+            gameArea.keys[e.keyCode] = false;
+        })
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+//.........BALLS....................................................................................//
+let by = 200;
+let dy = 0 ;
+let distance = 0;
+
+let bI = Math.floor(Math.random() * 4);
+let ballColor = color[bI];
+
+function gameBall(x, y, color) {
+    this.canvasHeight = gameArea.canvas.height;
+    this.x = x;
+    this.color = color;
+    this.y = y;    
+    ctx = gameArea.context;
+    this.update = function(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 10, 0, 2*Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    //...........BALL - A...............
+        let x = dy;
+        if (gameArea.keys && gameArea.keys[87]) {
+            dy = 2;
+            distance = 0;
+        }
+        if(gamePiece.y < Math.floor(gamePiece.canvasHeight/2)) {     
+            by += 8;   
+        } 
+        if((gamePiece.y >= gamePiece.canvasHeight - 15) && (gamePiece.y <= gamePiece.canvasHeight - 13)) {
+            dy = 0;
+        }
+        if((distance == 8) || (gamePiece.y < Math.floor(gamePiece.canvasHeight/2))) {     
+            dy = -1;     
+        }
+        gamePiece.y -= 3*x; 
+        distance++;
+    }
+}
+
+//........................OBSTACLES.........................//
+let c = 0;
+var angle = 0;
+let Obstacles = [
+    function circleObs (x, by,r, distanceObs, d) {
+        this.r = r;
+        this.direction = d;
+        this.distanceObs = distanceObs;
+        this.x = x;
+        this.y = by + this.distanceObs;
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(this.direction*c*Math.PI/180);
+        ctx.translate(-this.x,-this.y);
+        for(var i = 0; i <= 3; i++) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.r, (0 + angle)*Math.PI/180, (90 + angle)*Math.PI/180);
+            angle += 90;
+            ctx.lineWidth = 20;
+            ctx.strokeStyle = color[i];
+            ctx.stroke();
+            ctx.closePath();
+        }
+        ctx.restore();
+    },
+    function plusObs (x, by, r, distanceObs, d) {
+        this.r = r;
+        this.direction = d;
+        this.distanceObs = distanceObs;
+        this.y = by + distanceObs;
+        this.x = x + this.r/1.3*this.direction;
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(this.direction*c*Math.PI/180);
+        ctx.translate(-this.x,-this.y);
+        for(var i = 0; i <= 3; i++) {
+            ctx.beginPath();
+            ctx.translate(this.x, this.y);
+            ctx.rotate(angle*Math.PI/180);
+            ctx.translate(-this.x, -this.y);
+            angle += 90;
+            ctx.moveTo(this.x - this.r, this.y);
+            // this.parts == 2 ? ctx.lineTo(this.x + this.r, this.y) : ctx.lineTo(this.x, this.y);
+            ctx.lineTo(this.x, this.y)
+            ctx.lineCap = "round";
+            ctx.lineWidth = 20;
+            ctx.strokeStyle = color[i];
+            ctx.stroke();
+            ctx.closePath();
+        }
+        ctx.restore();
+    }
+];
+
+let pixel1;
+let pixel2;
+let rgb1;
+let rgb2;
+
+function collision() {
+    pixel1 = ctx.getImageData(gamePiece.x - 3, gamePiece.y - 2, 6, 1).data;
+    pixel2 = ctx.getImageData(gamePiece.x - 3, gamePiece.y + 2, 6, 1).data;
+
+    rgb1 = 'rgb(' + pixel1[0] + ', ' + pixel1[1] +', ' + pixel1[2] +')';
+    rgb2 = 'rgb(' + pixel2[0] + ', ' + pixel2[1] +', ' + pixel2[2] +')';
+    
+    if(rgb1 != rgb || rgb2 != rgb) {
+        if((pixel1[0] >= rMin[bI] && pixel1[1] >= gMin[bI] && pixel1[2] >= bMin[bI] && pixel1[0] <= rMax[bI] && pixel1[1] <= gMax[bI] && pixel1[2] <= bMax[bI]) || (pixel2[0] >= rMin[bI] && pixel2[1] >= gMin[bI] && pixel2[2] >= bMin[bI] && pixel2[0] <= rMax[bI] && pixel2[1] <= gMax[bI] && pixel2[2] <= bMax[bI])) {
+            gameRunning = gameRunning;
+        }
+        else {
+            gameRunning = !gameRunning;
+            gameOver.style.display = 'flex';
+            playerA.innerHTML = 'You Lost';
+            playerA.style.color = 'red';
+            playerB.innerHTML = 'You Won';
+            playerB.style.color = 'green';
+        }
+    } 
+}
+
+function gamePause() {
+gameRunning = !gameRunning;
+gameRunningB = !gameRunningB;
+if(gameRunning) {
+    updateGameArea();
+}
+if(gameRunningB) {
+    updateGameAreaB();
+}
+}
+//....................UPDATE GAME AREA...........................
+var i = 0;
+var j = 0;
+var r = 0;
+var direcType = [];
+var obsType = [];
+var radType = []; 
+var disType = [];
+var myArr = [0,1,2,3];
+var someArr = [1,-1];
+var t = 4;
+for(var k = 0; k < myArr.length; k++) {
+    j = Math.floor(Math.random() * 2);
+    r = Math.floor(Math.random() * (151 - 90)) + 90;
+    direcType.push(someArr[j]);
+    obsType.push(j);
+    radType.push(r); 
+    disType.push(i);
+    i -= 400;
+}
+function updateGameArea() {
+    gameArea.clear();
+    if(Obstacles.y > -10) {
+        direcType.shift();
+            direcType.push(someArr[j]);
+        obsType.shift();
+            obsType.push(Math.floor(Math.random() * 2));
+        radType.shift();
+            radType.push(Math.floor(Math.random() * (151 - 90)) + 90);
+        disType.shift();
+            disType.push(i);
+        i -= 400;
+    }
+    //....PLAYER - A
+    for(var k = 0; k < myArr.length; k++) {
+        Obstacles[obsType[myArr[k]]](gamePiece.x, by, radType[myArr[k]], disType[myArr[k]], direcType[myArr[k]]);
+    }
+    if(Obstacles.y > 0) {   
+        myArr.shift();  
+        myArr.push(t);  
+        t++;    
+    }
+    c += 1;
+    if(c == 360) {  
+        c = 0; 
+    }
+    if(angle == 360) {  
+        angle = 0; 
+    }
+    collision();
+    gamePiece.update();
+    if(gameRunning) {
+        requestAnimationFrame(updateGameArea);
+    }
+}
---
+++ playerB.js
@@ -0,0 +1,214 @@
+// 'use strict'
+let gamePieceB;
+function startGameB() {
+    gameAreaB.startB();
+    gamePieceB = new gameBallB(200, gameAreaB.canvasB.height -  250, ballColorB); 
+}
+//........GAME AREA LOAD.........//
+let gameAreaB = {
+    canvasB : document.createElement('canvas'),
+    startB : function() {
+        this.canvasB.width = 400;
+        this.canvasB.height = window.innerHeight;
+        this.canvasB.style.backgroundColor = 'rgb(59, 59, 59)';
+        this.context = this.canvasB.getContext('2d');
+        document.body.insertBefore(this.canvasB, document.body.childNodes[1]);
+        requestAnimationFrame(updategameAreaB);
+        document.addEventListener('keydown', function (e) {
+            gameAreaB.keys = (gameAreaB.keys || []);
+            gameAreaB.keys[e.keyCode] = true;
+        })
+        document.addEventListener('keyup', function (e) {
+            gameAreaB.keys[e.keyCode] = false;
+        })
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvasB.width, this.canvasB.height);
+    }
+}
+//.........BALLS....................................................................................//
+
+let byB = 200;
+let dyB = 0 ;
+let distanceB = 0;
+
+let bIB = Math.floor(Math.random() * 4);
+let ballColorB = color[bIB];
+
+function gameBallB(x, y, color) {
+    this.canvasHeightB = gameAreaB.canvasB.height;
+    this.x = x;
+    this.color = color;
+    this.y = y;    
+    ctxB = gameAreaB.context;
+    this.update = function(){
+        ctxB.beginPath();
+        ctxB.arc(this.x, this.y, 10, 0, 2*Math.PI);
+        ctxB.fillStyle = this.color;
+        ctxB.fill();
+        ctxB.closePath();
+    //...........BALL - A...............
+        let xB = dyB;
+        if (gameAreaB.keys && gameAreaB.keys[38]) {
+            dyB = 2;
+            distanceB = 0;
+        }
+        if(gamePieceB.y < Math.floor(gamePieceB.canvasHeightB/2)) {     
+            byB += 8;   
+        } 
+        if((gamePieceB.y >= gamePieceB.canvasHeightB - 15) && (gamePieceB.y <= gamePieceB.canvasHeightB - 13)) {
+            dyB = 0;
+        }
+        if((distanceB == 8) || (gamePieceB.y < Math.floor(gamePieceB.canvasHeightB/2))) {     
+            dyB = -1;     
+        }
+        gamePieceB.y -= 3*xB; 
+        distanceB++;
+    }
+}
+
+//........................OBSTACLES.........................//
+let cB = 0;
+var angleB = 0;
+let ObstaclesB = [
+    function circleObsB (x, by,r, distanceObs, d) {
+        this.r = r;
+        this.direction = d;
+        this.distanceObs = distanceObs;
+        this.x = x;
+        this.y = by + this.distanceObs;
+        ctxB.save();
+        ctxB.translate(this.x,this.y);
+        ctxB.rotate(this.direction*cB*Math.PI/180);
+        ctxB.translate(-this.x,-this.y);
+        for(var i = 0; i <= 3; i++) {
+            ctxB.beginPath();
+            ctxB.arc(this.x, this.y, this.r, (0 + angleB)*Math.PI/180, (90 + angleB)*Math.PI/180);
+            angleB += 90;
+            ctxB.lineWidth = 20;
+            ctxB.strokeStyle = color[i];
+            ctxB.stroke();
+            ctxB.closePath();
+        }
+        ctxB.restore();
+    },
+    function plusObsB (x, by, r, distanceObs, d) {
+        this.r = r;
+        this.direction = d;
+        this.distanceObs = distanceObs;
+        this.y = by + distanceObs;
+        this.x = x + this.r/1.3*this.direction;
+        ctxB.save();
+        ctxB.translate(this.x,this.y);
+        ctxB.rotate(this.direction*cB*Math.PI/180);
+        ctxB.translate(-this.x,-this.y);
+        for(var i = 0; i <= 3; i++) {
+            ctxB.beginPath();
+            ctxB.translate(this.x, this.y);
+            ctxB.rotate(angleB*Math.PI/180);
+            ctxB.translate(-this.x, -this.y);
+            angleB += 90;
+            ctxB.moveTo(this.x - this.r, this.y);
+            // this.parts == 2 ? ctx.lineTo(this.x + this.r, this.y) : ctx.lineTo(this.x, this.y);
+            ctxB.lineTo(this.x, this.y)
+            ctxB.lineCap = "round";
+            ctxB.lineWidth = 20;
+            ctxB.strokeStyle = color[i];
+            ctxB.stroke();
+            ctxB.closePath();
+        }
+        ctxB.restore();
+    }
+];
+
+// let pixel3;
+// let pixel4;
+let rgb3;
+let rgb4;
+
+function collisionB() {
+    pixel3 = ctxB.getImageData(gamePieceB.x - 3, gamePieceB.y - 2, 6, 1).data;
+    pixel4 = ctxB.getImageData(gamePieceB.x - 3, gamePieceB.y + 2, 6, 1).data;
+
+    rgb3 = 'rgb(' + pixel3[0] + ', ' + pixel3[1] +', ' + pixel3[2] +')';
+    rgb4 = 'rgb(' + pixel4[0] + ', ' + pixel4[1] +', ' + pixel4[2] +')';
+    
+    if(rgb3 != rgb || rgb4 != rgb) {
+        if((pixel3[0] >= rMin[bIB] && pixel3[1] >= gMin[bIB] && pixel3[2] >= bMin[bIB] && pixel3[0] <= rMax[bIB] && pixel3[1] <= gMax[bIB] && pixel3[2] <= bMax[bIB]) || (pixel4[0] >= rMin[bIB] && pixel4[1] >= gMin[bIB] && pixel4[2] >= bMin[bIB] && pixel4[0] <= rMax[bIB] && pixel4[1] <= gMax[bIB] && pixel4[2] <= bMax[bIB])) {
+            gameRunning = gameRunning;
+        }
+        else {
+            gameRunning = !gameRunning;
+            gameOver.style.display = 'flex';
+            playerB.innerHTML = 'You Lost';
+            playerB.style.color = 'red';
+            playerA.innerHTML = 'You Won';
+            playerA.style.color = 'green';
+        }
+    }
+    console.log(rgb3);
+    console.log(rgb4);
+    
+}
+
+function gamePauseB() {
+    gameRunningB = !gameRunningB;
+    if(gameRunningB) {
+        updateGameAreaB();
+    }
+}
+//....................UPDATE GAME AREA...........................
+var iB = 0;
+var jB = 0;
+var rB = 0;
+var direcTypeB = [];
+var obsTypeB = [];
+var radTypeB = []; 
+var disTypeB = [];
+var myArrB = [0,1,2,3];
+var someArrB = [1,-1];
+var tB = 4;
+for(var k = 0; k < myArrB.length; k++) {
+    jB = Math.floor(Math.random() * 2);
+    rB = Math.floor(Math.random() * (151 - 90)) + 90;
+    direcTypeB.push(someArrB[jB]);
+    obsTypeB.push(jB);
+    radTypeB.push(rB); 
+    disTypeB.push(iB);
+    iB -= 400;
+}
+function updategameAreaB() {
+    gameAreaB.clear();
+    if(ObstaclesB.y > -10) {
+        direcTypeB.shift();
+            direcTypeB.push(someArrB[jB]);
+        obsTypeB.shift();
+            obsTypeB.push(Math.floor(Math.random() * 2));
+        radTypeB.shift();
+            radTypeB.push(Math.floor(Math.random() * (151 - 90)) + 90);
+        disTypeB.shift();
+            disTypeB.push(iB);
+        iB -= 400;
+    }
+    //....PLAYER - A
+    for(var k = 0; k < myArrB.length; k++) {
+        ObstaclesB[obsTypeB[myArrB[k]]](gamePieceB.x, byB, radTypeB[myArrB[k]], disTypeB[myArrB[k]], direcTypeB[myArrB[k]]);
+    }
+    if(ObstaclesB.y > 0) {   
+        myArrB.shift();  
+        myArrB.push(tB);  
+        tB++;    
+    }
+    cB += 1;
+    if(cB == 360) {  
+        cB = 0; 
+    }
+    if(angleB == 360) {  
+        angleB = 0; 
+    }
+    collisionB();
+    gamePieceB.update();
+    if(gameRunning) { 
+    requestAnimationFrame(updategameAreaB);
+    }
+}
